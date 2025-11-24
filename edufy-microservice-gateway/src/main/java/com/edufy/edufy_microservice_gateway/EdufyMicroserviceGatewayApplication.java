a315f5d360e49d629d15bdb590bc54faae17c7fb
samuel.funck <samuel.funck@outlook.com>
2025-11-24 12:25:56 +0100

    Init commit for gateway

---
+++ edufy-microservice-gateway/src/main/java/com/edufy/edufy_microservice_gateway/EdufyMicroserviceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.edufy.edufy_microservice_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdufyMicroserviceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdufyMicroserviceGatewayApplication.class, args);
+	}
+
+}
---
+++ edufy-microservice-gateway/src/test/java/com/edufy/edufy_microservice_gateway/EdufyMicroserviceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edufy.edufy_microservice_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdufyMicroserviceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
